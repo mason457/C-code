b7d1a379780da7aa17b5dd0657ecef783910abd3
mason457 <88136451+mason457@users.noreply.github.com>
2021-07-31 09:50:36 +0800

    Update 9by9.cpp

--- 9by9.cpp
+++ 9by9.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
-void main()
+#include <stdlib.h>
+int main()
 {
-    int value;
     for(int i = 1;i <=9; i++)
     {
         for(int j = 1;j <=9; j++)
         {
-            printf("%2d",i*j);
+            printf("%3d",i*j);
         }
         printf("\n");
     }
+    system("pause");
+    return 0;
 }
